c5f336f9d819c601f1ad1bf0135ac7cc552a2142
Olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2025-06-27 07:51:57 +0100

    fix: naming inconsistencies and add "getAllUsers" endpoint

--- client/src/types/Auth.tsx
+++ client/src/types/Auth.tsx
@@ -1,9 +1,9 @@
-// src/types/auth.ts
 export type UserRole = 'citizen' | 'officer' | 'supervisor';
 export type AuthContextType = {
   role: UserRole;
   isAuthenticated: boolean;
   isLogin: boolean;
+  login: (token: string, userRole: UserRole) => void;
   setRole: (role: UserRole) => void;
   setIsAuthenticated: (auth: boolean) => void;
   setIsLogin: (isLogin: boolean) => void;
